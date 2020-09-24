a1feec72b6960430fdd21339ada9a117eb8bb929
Ryan K Burns <ryankburns@gmail.com>
2020-09-24 17:52:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,3 +17,9 @@ end
 def swap_elements(array)
     array[0], array[1], array[2] = array[0], array[2], array[1]
 end
+
+def reverse_array(array)
+  array.sort do |a, b|
+    array.reverse
+  end
+end
