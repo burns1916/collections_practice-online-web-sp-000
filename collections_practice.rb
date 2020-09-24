96d251e1a93b490177b6bb8ee03a38321af3fdfc
Ryan K Burns <ryankburns@gmail.com>
2020-09-24 17:55:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,9 +15,9 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-    array[0], array[1], array[2] = array[0], array[2], array[1]
+  array[0], array[1], array[2] = array[0], array[2], array[1]
 end
 
 def reverse_array(array)
-    array == array.reverse
+  array.reverse(array)
 end
